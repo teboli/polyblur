7f2084c00406ad7ce342848752b67bdd4a6089b6
teboli <thomas.eboli@ens-paris-saclay.fr>
2022-10-11 10:23:07 +0200

    release 0.1.2

--- setup.py
+++ setup.py
@@ -1,21 +1,34 @@
 from setuptools import setup, find_packages, Extension
 from torch.utils import cpp_extension
-import re
 
-# # Install the intermediate separable kernel
-# setup(name='separable_gaussian2d',
-#         ext_modules=[cpp_extension.CppExtension('separable_gaussian2d', ['./polyblur/separable_convolution/separable_gaussian2d.cpp'])],
-#         cmdclass={'build_ext': cpp_extension.BuildExtension})
+long_description = '''# Polyblur Python package
 
+Pytorch-based implementation of "Polyblur: Removing mild blur by polynomial reblurring" [Delbracio2021] and detailed in "Breaking down Polyblur: Fast blind correction of small anisotropic blurs" [Eboli2022]. Any question at thomas.eboli@ens-paris-saclay.fr.
 
-with open("long_description.txt", "r") as fh:
-    long_description = fh.read()
+Import the package as
+
+```shell
+import polyblur
+```
+
+In your Python script, run the functional interface
+
+```shell
+polyblur.polyblur_deblurring
+```
+
+or the torch.nn.Module interface
+
+```shell
+polyblur.PolyblurDeblurring
+```
+'''
 
 
 # Install the main polyblur module
 setup(
     name='polyblur',
-    version="1.0.1",
+    version="0.1.2",
     author="Thomas Eboli",
     author_email="thomas.eboli@ens-paris-saclay.fr",
     description="Breaking down Polyblur: Fast blind Correction of Small Anisotropic Blurs [IPOL2022]",
@@ -23,7 +36,6 @@ setup(
     long_description=long_description,
     packages = find_packages(),
     include_package_data=True,
-    install_requires=["torch-tools"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -33,3 +45,5 @@ setup(
         "Topic :: Scientific/Engineering :: Image Processing",
     ],
 )
+
+
